f7e5457dce87da30fb8c16e44e4fca5b6039a09b
FlyingFox365 <tristanhallc2@icloud.com>
2025-10-30 15:24:24 +0000

    Made Single interface with Music and Implemented getPlayingTime

--- src/Single.kt
+++ src/Single.kt
@@ -1,2 +1,10 @@
-class Single(val aSide: Song, val bSide: Song) {
+class Single(val aSide: Song, val bSide: Song): Music {
+    override fun getPlayingTime(): Double {
+        val playTime = aSide.playingTime + bSide.playingTime
+        return playTime
+    }
+
+    override fun getAllSongs(): List<Song> {
+        TODO("Not yet implemented")
+    }
 }
