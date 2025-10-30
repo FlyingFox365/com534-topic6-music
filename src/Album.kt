de67d153e7c026e9f4854516db591e9db26e5754
FlyingFox365 <tristanhallc2@icloud.com>
2025-10-30 15:11:38 +0000

    Added Music interface and filter method for Album class

--- src/Album.kt
+++ src/Album.kt
@@ -1,3 +1,12 @@
-class Album(val songs: List<Song>) {
+class Album(val songs: List<Song>): Music {
+    override fun getPlayingTime(): Double {
+        var
+        for (song in songs) {
 
+        }
+    }
+
+    fun filterByArtist(artist: String): List<Song> {
+        return songs.filter { it.artist == artist }
+    }
 }
